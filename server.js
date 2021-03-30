966bf73ff067fb0ca7d46f56ce8ccaabc2773789
Manuel <manuel.astudillo.rojass@gmail.com>
2021-03-30 09:02:56 -0300

    primera subida

--- server.js
+++ server.js
@@ -12,17 +12,6 @@ app.use(express.static(__dirname + '/public'));
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
-hbs.registerHelper('getAnio', () => {
-    return new Date().getFullYear;
-});
-hbs.registerHelper('capitalizar', (texto) => {
-    let palabras = texto.split(' ');
-    palabras.forEach((palabra, idx) => {
-        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
-    });
-
-    return palabras.join(' ');
-})
 
 
 
